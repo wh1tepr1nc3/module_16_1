19b8dcc1658a70e619da0fe73eb6e1f69bf558d1
wh1tepr1nc3 <amamamania@mail.ru>
2024-12-21 16:28:18 +0500

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -1,20 +1,21 @@
+# uvicorn module_16_1:app --reload
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 @app.get("/")
-async def get_main_page() -> dict:
+async def get_main_page():
     return {"message": "Главная страница"}
 
 
 @app.get("/user/admin")
-async def get_admin_page() -> dict:
+async def get_admin_page():
     return {"message": "Вы вошли как администратор"}
 
 
 @app.get("/user/{user_id}")
-async def get_user_number(user_id: str) -> dict:
+async def get_user_number(user_id: str):
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
 
